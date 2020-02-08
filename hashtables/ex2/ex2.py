f2df1714a027f0350eca702faf7014d674d19e34
Kristea Gamel <krgamel@gmail.com>
2020-02-08 12:30:48 -0600

    hash2 passes tests.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,19 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
+    sorted = False
+    source = 'NONE'
+    index = 0
+    while not sorted:
+        destination = hash_table_retrieve(hashtable, source)
+        route[index] = destination
+        index += 1
+        if destination != 'NONE':
+            source = destination
+        else:
+            sorted = True
+        print(route)
     return route

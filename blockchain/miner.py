fecc1e63a833718c30675efdd86e438cb8af9d4d
Kristea Gamel <krgamel@gmail.com>
2020-02-08 11:59:09 -0600

    miner is mining

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -7,7 +7,7 @@ from uuid import uuid4
 
 from timeit import default_timer as timer
 
-import random
+from random import randint
 
 
 def proof_of_work(last_proof):
@@ -23,8 +23,17 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    # proof = 0
     #  TODO: Your code here
+    # setting proof to last_proof times a random int between 0 and 100
+    proof = last_proof*randint(0, 100)
+
+    new_last_proof = f'{last_proof}'.encode()
+
+    last_hash = hashlib.sha256(new_last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
